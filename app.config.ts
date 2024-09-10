81e15811f020ce111ca39bd480ce193d3e811c37
stockimen <stockimen@qq.com>
2024-09-10 13:19:08 +0800

    Auto commit at 2024-09-10 13:19:08

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,7 @@
 export default defineAppConfig({
   title: 'Sink',
   description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
-  image: 'https://sink.cool/banner.png',
+  image: 'https://sink.739527.xyz/banner.png',
   previewTTL: 24 * 3600, // 24h
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
   reserveSlug: [
--- server/api/verify.ts
+++ server/api/verify.ts
@@ -1,6 +1,6 @@
 export default eventHandler(() => {
   return {
     name: 'Sink',
-    url: 'https://sink.cool',
+    url: 'https://sink.739527.xyz',
   }
 })
